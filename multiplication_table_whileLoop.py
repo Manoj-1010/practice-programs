7e365d37828d8c0f3d34a1015be8a873279fadee
Manoj <132181823+Manoj-1010@users.noreply.github.com>
2025-01-21 21:59:48 +0530

    Create multiplication_table_whileLoop.py

---
+++ multiplication_table_whileLoop.py
@@ -0,0 +1,12 @@
+# Function to generate the multiplication table
+def multiplication_table(n, s): # Args: n = Multiplicant; s = Multiplier limit
+    i = 1
+    while i <= s: # While loop for generating the multiplication table
+        print(f"{n} x {i} = {n*i}")
+        i += 1
+
+# Getting user inputs for 'n' and 's'
+n = int(input("Enter the Multiplicant: "))
+s = int(input("Enter the Multiplier limit: "))
+
+multiplication_table(n,s)
