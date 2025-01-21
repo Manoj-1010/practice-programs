7b2e506362528743185c5b4b727f43306971617b
Manoj <132181823+Manoj-1010@users.noreply.github.com>
2025-01-21 21:35:10 +0530

    Add files via upload

---
+++ first_to_last.py
@@ -0,0 +1,9 @@
+# Function for printing numbers from first to last
+def first_to_last(first, last):
+    if first > last: print(-1); return # Return -1 if first is greater than large
+    while first <= last: # Loop statement for printing the series
+        print(first, end=" ")
+        first += 1
+
+first_to_last(1,10) # Output: 1 2 3 4 5 6 7 8 9 10
+first_to_last(7,5) # Output: -1
