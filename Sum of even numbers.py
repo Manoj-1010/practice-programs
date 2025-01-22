e5a8a6ee8348f2c7ff02f7099311e49320325ffe
Manoj <132181823+Manoj-1010@users.noreply.github.com>
2025-01-22 18:52:51 +0530

    Delete Sum of even numbers.py

--- Sum of even numbers.py
+++
@@ -1,8 +0,0 @@
-num=[i for i in range(1,11)]
-even=[]
-print("ORIGINAL LIST: ",num)
-for j in num:
-    if j%2==0:
-        even.append(j)
-print("THE SUM OF EVEN NUMBERS IN THE LIST: ",sum(even))
-
