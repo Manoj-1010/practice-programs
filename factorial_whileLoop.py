32eda7f9b524d1476c1c65bd1d5aa3d5333ff0ff
Manoj <132181823+Manoj-1010@users.noreply.github.com>
2025-01-22 18:44:59 +0530

    Create factorial_whileLoop.py

---
+++ factorial_whileLoop.py
@@ -0,0 +1,13 @@
+def factorial(n):
+    if n < 0: return -1 # Return -1 for negative numbers, since factorial is undefined
+    fact, i = 1, 1
+
+    while i<= n:
+        fact *= i # Multiplying the existing value and assigning the product to itself
+        i += 1
+    
+    return fact
+
+print(factorial(5)) # Output: 120
+print(factorial(10)) # Output: 3628800
+print(factorial(-5)) # Output: -1
