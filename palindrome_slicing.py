ea30d35399bba6ad10702fd5278fc5de7dea56ca
Manoj <132181823+Manoj-1010@users.noreply.github.com>
2025-01-28 16:20:19 +0530

    Update palindrome_slicing.py

--- palindrome_slicing.py
+++ palindrome_slicing.py
@@ -1,6 +1,6 @@
 def isPalindrome(string):
     # Comparing the original string, and it's reversed string, we can determine if it is a Palindrome or not
-    if string == string[::-1]:
+    if string.lower() == string[::-1].lower():
         print(f"{string.capitalize()} is a palindrome")
     else:
         print(f"{string.capitalize()} is not a palindrome")
