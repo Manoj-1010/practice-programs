5a118d9699419c949ca2f9978b12b8cf2b85f227
Manoj <132181823+Manoj-1010@users.noreply.github.com>
2025-01-28 16:13:58 +0530

    Create palindrome_slicing.py

---
+++ palindrome_slicing.py
@@ -0,0 +1,9 @@
+def isPalindrome(string):
+    # Comparing the original string, and it's reversed string, we can determine if it is a Palindrome or not
+    if string == string[::-1]:
+        print(f"{string.capitalize()} is a palindrome")
+    else:
+        print(f"{string.capitalize()} is not a palindrome")
+
+if __name__ == "__main__":
+    isPalindrome(input())
